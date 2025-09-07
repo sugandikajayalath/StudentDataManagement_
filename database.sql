bb059a8fb8f040d8be07de0772dbeb85b8eb246a
sugandikajayalath <sugandikaerandi8@gmail.com>
2025-09-07 17:49:13 +0100

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,12 @@
+SHOW DATABASES;
+CREATE DATABASE studentdb;
+
+USE studentdb;
+
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    grade VARCHAR(10) NOT NULL
+);
+SHOW TABLES;
